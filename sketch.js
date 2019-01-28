fd48e54d6d29862c3c2ef8ba42aeba167a3484e9
Lorraine Li <yli06@risd.edu>
2019-01-28 01:53:23 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var capture;
+
+function setup() {
+  createCanvas(640, 480);
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: "user"
+    }
+  };
+  capture = createCapture(constraints);
+}
+function draw() {
+  image(capture, 0, 0, 640, 480);
+  // filter(INVERT); // ;-)
+}
