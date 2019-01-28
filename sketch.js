3144f18f5cef18e5207c9461ea2a36cbaa9ab3ef
Lorraine Li <yli06@risd.edu>
2019-01-28 05:22:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,31 +26,31 @@ function setup() {
 }
 function draw() {
   background(255);
-  image(capture,0,0,60,55);
- // capture.loadPixels();
-  // for (var x = 0; x < cwidth; x++) {
-  //   for (var y = 0; y < cwidth; y+=10) {
-  //     var index = (x+y+cwidth)*4;
-  //     var r = capture.pixels[index+0];
-  //     var g = capture.pixels[index+1];
-  //     var b = capture.pixels[index+2];
-  //     var bright = (r+g+b)/3;
+  image(capture,0,0);
+  capture.loadPixels();
+  for (var x = 0; x < cwidth; x++) {
+    for (var y = 0; y < cwidth; y+=10) {
+      var index = (x+y+cwidth)*4;
+      var r = capture.pixels[index+0];
+      var g = capture.pixels[index+1];
+      var b = capture.pixels[index+2];
+      var bright = (r+g+b)/3;
       
-  //     noStroke();
-  //     fill(255);
-  //     rect(x*pw,y+ph,pw,ph-pw);
-  //     stroke(0);
-  //     line(x*pw,y*ph,x*pw+pw,y*ph);
-  //     line(x*pw+pw/2,y*ph,x*pw+pw/2,y*ph+ph);
+      noStroke();
+      fill(255);
+      rect(x*pw,y+ph,pw,ph-pw);
+      stroke(0);
+      line(x*pw,y*ph,x*pw+pw,y*ph);
+      line(x*pw+pw/2,y*ph,x*pw+pw/2,y*ph+ph);
       
       
-  //     if (bright < threshold) {
-  //       noStroke();
-  //       fill(20);
-  //       rect(x*pw+pw/4,y*ph,pw/2,ph/2);
+      if (bright < threshold) {
+        noStroke();
+        fill(20);
+        rect(x*pw+pw/4,y*ph,pw/2,ph/2);
 
-  //     }
-  //   }
-  // }
+      }
+    }
+  }
   
 }
