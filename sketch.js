e02c196a94c560ef707915db84b4ee44751c45c1
Lorraine Li <yli06@risd.edu>
2019-01-28 03:49:46 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,21 +4,11 @@ var capture;
 
 function setup() {
   createCanvas(640, 480);
-  var constraints = {
-    audio: false,
-    video: {
 
-      facingMode: "user"
-
-    }
-  };
-  capture = createCapture(constraints);
-    capture.elt.setAttribute('playsinline', '');
 }
 function draw() {
   background(40);
 
-  image(capture, 0, 0, windowWidth, windowHeight);
-  //filter(INVERT);
+
 }
 
