41e3da05f77c846d61abfe2085c1928875fe03bb
Lorraine Li <yli06@risd.edu>
2019-01-28 05:10:06 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 var capture;
 var cwidth = 60;
 var cheight = 55;
+var pw=6;
+var ph=pw*5;
+
 
 function setup() {
   createCanvas(764,1024);
@@ -22,21 +25,28 @@ function setup() {
 }
 function draw() {
   capture.loadPixels();
-  for (var y = 0; y < cheight; y++) {
-    for (var x = 0; x < cwidth; x+=10) {
+  for (var x = 0; x < cwidth; x++) {
+    for (var y = 0; y < cwidth; y+=10) {
       var index = (x+y+cwidth)*4;
       var r = capture.pixels[index+0];
       var g = capture.pixels[index+1];
       var b = capture.pixels[index+2];
       var bright = (r+g+b)/3;
       
-      line(y,x,y,x+6);
+      noStroke();
+      fill(255);
+      rect(x*pw,y+ph,pw,ph-pw);
+      stroke(0);
+      line(x*pw,y*ph,x*pw+pw,y*ph);
+      line(x*pw+pw/2,y*ph,x*pw+pw/2,y*ph+ph);
       
       
-      if (bright >= threshold) {
-        
+      if (bright < threshold) {
+        noStroke();
+        fill(20);
+        rect(x*pw+pw/4,y*ph,pw/2,ph/w);
+
       }
-      
     }
   }
   
