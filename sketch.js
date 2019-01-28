133fd516a93b12d1fa7c2badb64e22b5590f91bb
Lorraine Li <yli06@risd.edu>
2019-01-28 05:00:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,43 @@
 var capture;
+var cwidth = 60;
+var cheight = 55;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(764,1024);
   var constraints = {
     audio: false,
     video: {
 
-      facingMode: "environment",
+      facingMode: "user",
       frameRate: 10
     }
   };
   capture = createCapture(constraints);
   capture.elt.setAttribute('playsinline', '');
+  capture.size(60,55);
   capture.hide();
+  var threshold = 50;
+  strokeWeight(2);
+  
 }
 function draw() {
-  image(capture, 0, 0, windowWidth, windowHeight);
-  //filter(INVERT);
-}x
+  capture.loadPixels();
+  for (var y = 0; y < cheight; y++) {
+    for (var x = 0; x < cwidth; x+=10) {
+      var index = (x+y+cwidth)*4;
+      var r = capture.pixels[index+0];
+      var g = capture.pixels[index+1];
+      var b = capture.pixels[index+2];
+      var bright = (r+g+b)/3;
+      
+      line(y,x,y,x+6);
+      
+      
+      if (bright >= threshold) {
+        
+      }
+      
+    }
+  }
+  
+}
